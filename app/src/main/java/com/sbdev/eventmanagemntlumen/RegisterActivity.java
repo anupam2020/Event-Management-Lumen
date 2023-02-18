547f005667cf7c7b27f429b2741c9437240533bd
Anupam Basak <anupam00basak@gmail.com>
2023-02-18 11:53:01 +0530

    Register Backend done!

--- app/src/main/java/com/sbdev/eventmanagemntlumen/RegisterActivity.java
+++ app/src/main/java/com/sbdev/eventmanagemntlumen/RegisterActivity.java
@@ -1,16 +1,96 @@
 package com.sbdev.eventmanagemntlumen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private TextInputEditText name,email,pass;
+    private AppCompatButton register;
+
+    private DatabaseReference dRef;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        name=findViewById(R.id.registerName);
+        email=findViewById(R.id.registerEmail);
+        pass=findViewById(R.id.registerPassword);
+        register=findViewById(R.id.registerBtn);
+
+        dRef= FirebaseDatabase.getInstance().getReference("Users");
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String n=name.getText().toString();
+                String e=email.getText().toString();
+                String p=pass.getText().toString();
+
+                if(n.isEmpty() || e.isEmpty() || p.isEmpty())
+                {
+                    Toast.makeText(RegisterActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    pushToFirebase(n,e,p);
+                }
+
+            }
+        });
+
+
+    }
+
+    private void pushToFirebase(String name, String email, String pass)
+    {
+
+        firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+
+                    HashMap<String,String> map=new HashMap<>();
+                    map.put("Name",name);
+                    map.put("Email",email);
+
+                    dRef.setValue(map);
+
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
